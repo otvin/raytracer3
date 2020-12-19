45b8a6fac38f5812069b1b89b1be4e60bea7d276
otvin <michael_fredericks@hotmail.com>
2020-12-19 13:55:11 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Third raytracer
+
+Built using _The Ray Tracer Challenge_ by Jamis Buck
+
---
+++ unit_tests.py
@@ -0,0 +1,7 @@
+import raytracer3
+
+def run_tests():
+    print('hi')
+
+if __name__ == '__main__':
+    run_tests()
