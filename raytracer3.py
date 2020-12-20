d7f227f8de11c91fb547865bcc1d3f4b549e1a1e
otvin <michael_fredericks@hotmail.com>
2020-12-19 16:11:02 -0800

    Additional unit tests

--- raytracer3.py
+++ raytracer3.py
@@ -3,8 +3,10 @@ import math
 
 
 class RT_Tuple:
-    def __init__(self, x, y, z, a):
-        self.arr = np.array([x, y, z, a])
+    def __init__(self, x=0.0, y=0.0, z=0.0, w=0.0):
+        self.arr = np.array([x, y, z, w])
+        # We will allow creation of tuples with integer arguments, but want to store as floats.
+        self.arr = self.arr.astype('float64')
 
     @property
     def x(self):
@@ -31,33 +33,56 @@ class RT_Tuple:
         self.arr[2] = z
 
     @property
-    def a(self):
+    def w(self):
         return self.arr[3]
 
-    @a.setter
-    def a(self, a):
-        self.arr[3] = a
+    @w.setter
+    def w(self, w):
+        self.arr[3] = w
 
     def __eq__(self, other):
         return np.allclose(self.arr, other.arr)
 
+    def __neg__(self):
+        res = RT_Tuple()
+        res.arr = -self.arr
+        return res
+
+    def __add__(self, other):
+        res = RT_Tuple()
+        res.arr = self.arr + other.arr
+        return res
+
+    def __sub__(self, other):
+        res = RT_Tuple()
+        res.arr = self.arr - other.arr
+        return res
+
+    def __mul__(self, other):
+        res = RT_Tuple()
+        if isinstance(other, RT_Tuple):
+            res.arr = self.arr * other.arr
+        else:
+            res.arr = self.arr * other
+        return res
+
     def ispoint(self):
-        return math.isclose(self.a, 1.0)
+        return math.isclose(self.w, 1.0)
 
     def isvector(self):
-        return math.isclose(self.a, 0.0)
+        return math.isclose(self.w, 0.0)
 
 
 
 
 
 class Point(RT_Tuple):
-    def __init__(self, x, y, z):
+    def __init__(self, x=0.0, y=0.0, z=0.0):
         super().__init__(x, y, z, 1.0)
 
 
 class Vector(RT_Tuple):
-    def __init__(self, x, y, z):
+    def __init__(self, x=0.0, y=0.0, z=0.0):
         super().__init__(x, y, z, 0.0)
 
 
--- unit_tests.py
+++ unit_tests.py
@@ -1,7 +1,9 @@
 import raytracer3 as rt
 import math
+import time
 
 def test_point1():
+    # A tuple with w=1.0 is a point
     t = rt.RT_Tuple(4.3, -4.2, 3.1, 1.0)
     assert math.isclose(t.x, 4.3)
     assert math.isclose(t.y, -4.2)
@@ -11,6 +13,7 @@ def test_point1():
 
 
 def test_vector1():
+    # A tuple with w=0 is a vector
     t = rt.RT_Tuple(4.3, -4.2, 3.1, 0.0)
     assert math.isclose(t.x, 4.3)
     assert math.isclose(t.y, -4.2)
@@ -20,15 +23,64 @@ def test_vector1():
 
 
 def test_point2():
+    # Point() creates tuples with w=1
     p = rt.Point(4.0, -4.0, 3.0)
     assert isinstance(p, rt.RT_Tuple)
     assert p == rt.RT_Tuple(4.0, -4.0, 3.0, 1.0)
 
 
 def test_vector2():
+    # Vector() creates tuples with w=0
     p = rt.Vector(4, -4, 3)
     assert isinstance(p, rt.RT_Tuple)
     assert p == rt.RT_Tuple(4.0, -4.0, 3.0, 0.0)
+
+
+def test_add1():
+    # Adding two tuples
+    p1 = rt.RT_Tuple(3, -2, 5, 1)
+    p2 = rt.RT_Tuple(-2, 3, 1, 0)
+    assert p1 + p2 == rt.RT_Tuple(1, 1, 6, 1)
+
+
+def test_sub1():
+    # Subtracting two points
+    p1 = rt.Point(3, 2, 1)
+    p2 = rt.Point(5, 6, 7)
+    assert p1 - p2 == rt.Vector(-2, -4, -6)
+
+
+def test_sub2():
+    # Subtracting a vector from a point
+    p = rt.Point(3, 2, 1)
+    v = rt.Vector(5, 6, 7)
+    assert p - v == rt.Point(-2, -4, -6)
+
+
+def test_sub3():
+    # Subtracting two vectors
+    v1 = rt.Vector(3, 2, 1)
+    v2 = rt.Vector(5, 6, 7)
+    assert v1 - v2 == rt.Vector(-2, -4, -6)
+
+
+def test_negation1():
+    # Subtracting a vector from the zero vector
+    zero = rt.Vector(0, 0, 0)
+    v = rt.Vector(1, -2, 3)
+    assert zero - v == rt.Vector(-1, 2, -3)
+
+
+def test_negation2():
+    # Negating a tuple
+    a = rt.RT_Tuple(1, -2, 3, -4)
+    assert -a == rt.RT_Tuple(-1, 2, -3, 4)
+
+
+def test_scalarmult1():
+    # Multiplying a tuple by a scalar
+    a = rt.RT_Tuple(1, -2, 3, -4)
+    assert a * 3.5 == rt.RT_Tuple(3.5, -7, 10.5, -14)
 
 
 def run_tests():
@@ -36,7 +88,13 @@ def run_tests():
     test_vector1()
     test_point2()
     test_vector2()
-
+    test_add1()
+    test_sub1()
+    test_sub2()
+    test_sub3()
+    test_negation1()
+    test_negation2()
+    test_scalarmult1()
 
 if __name__ == '__main__':
     run_tests()
