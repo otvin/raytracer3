e3a98e4016f3732f1ca8fa1d3da396bc4270ed21
otvin <michael_fredericks@hotmail.com>
2020-12-19 22:37:20 -0500

    Finished chapter 1

---
+++ tuple.py
@@ -0,0 +1,116 @@
+import numpy as np
+import math
+
+
+class RT_Tuple:
+    def __init__(self, x=0.0, y=0.0, z=0.0, w=0.0):
+        self.arr = np.array([x, y, z, w])
+        # We will allow creation of tuples with integer arguments, but want to store as floats.
+        self.arr = self.arr.astype('float64')
+
+    @property
+    def x(self):
+        return self.arr[0]
+
+    @x.setter
+    def x(self, x):
+        self.arr[0] = x
+
+    @property
+    def y(self):
+        return self.arr[1]
+
+    @y.setter
+    def y(self, y):
+        self.arr[1] = y
+
+    @property
+    def z(self):
+        return self.arr[2]
+
+    @z.setter
+    def z(self, z):
+        self.arr[2] = z
+
+    @property
+    def w(self):
+        return self.arr[3]
+
+    @w.setter
+    def w(self, w):
+        self.arr[3] = w
+
+    def __eq__(self, other):
+        return np.allclose(self.arr, other.arr)
+
+    def __neg__(self):
+        res = RT_Tuple()
+        res.arr = -self.arr
+        return res
+
+    def __add__(self, other):
+        res = RT_Tuple()
+        res.arr = self.arr + other.arr
+        return res
+
+    def __sub__(self, other):
+        res = RT_Tuple()
+        res.arr = self.arr - other.arr
+        return res
+
+    def __mul__(self, other):
+        res = RT_Tuple()
+        if isinstance(other, RT_Tuple):
+            res.arr = self.arr * other.arr
+        else:
+            res.arr = self.arr * other
+        return res
+
+    def __truediv__(self, other):
+        res = RT_Tuple()
+        if isinstance(other, RT_Tuple):
+            res.arr = self.arr / other.arr
+        else:
+            res.arr = self.arr / other
+        return res
+
+    def ispoint(self):
+        return math.isclose(self.w, 1.0)
+
+    def isvector(self):
+        return math.isclose(self.w, 0.0)
+
+
+class Point(RT_Tuple):
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        super().__init__(x, y, z, 1.0)
+
+
+class Vector(RT_Tuple):
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        super().__init__(x, y, z, 0.0)
+
+    def magnitude(self):
+        sqrlenarr = self.arr * self.arr
+        sqrlensum = np.sum(sqrlenarr[:3])
+        return math.sqrt(sqrlensum)
+
+
+def normalize(vec):
+    # returns a vector which is the normalized version of self
+    res = Vector()
+    mag = vec.magnitude()
+    res.arr = vec.arr / mag
+    return res
+
+
+def dot(tup1, tup2):
+    # returns dot product of the two tuples
+    return np.dot(tup1.arr, tup2.arr)
+
+
+def cross(vec1, vec2):
+    # cross product of two vectors
+    res = Vector()
+    res.arr[:3] = np.cross(vec1.arr[:3], vec2.arr[:3])
+    return res
--- unit_tests.py
+++ unit_tests.py
@@ -1,10 +1,10 @@
-import raytracer3 as rt
+import tuple
 import math
 
 
 def test_point1():
     # A tuple with w=1.0 is a point
-    t = rt.RT_Tuple(4.3, -4.2, 3.1, 1.0)
+    t = tuple.RT_Tuple(4.3, -4.2, 3.1, 1.0)
     assert math.isclose(t.x, 4.3)
     assert math.isclose(t.y, -4.2)
     assert math.isclose(t.z, 3.1)
@@ -14,7 +14,7 @@ def test_point1():
 
 def test_vector1():
     # A tuple with w=0 is a vector
-    t = rt.RT_Tuple(4.3, -4.2, 3.1, 0.0)
+    t = tuple.RT_Tuple(4.3, -4.2, 3.1, 0.0)
     assert math.isclose(t.x, 4.3)
     assert math.isclose(t.y, -4.2)
     assert math.isclose(t.z, 3.1)
@@ -24,116 +24,143 @@ def test_vector1():
 
 def test_point2():
     # Point() creates tuples with w=1
-    p = rt.Point(4.0, -4.0, 3.0)
-    assert isinstance(p, rt.RT_Tuple)
-    assert p == rt.RT_Tuple(4.0, -4.0, 3.0, 1.0)
+    p = tuple.Point(4.0, -4.0, 3.0)
+    assert isinstance(p, tuple.RT_Tuple)
+    assert p == tuple.RT_Tuple(4.0, -4.0, 3.0, 1.0)
 
 
 def test_vector2():
     # Vector() creates tuples with w=0
-    p = rt.Vector(4, -4, 3)
-    assert isinstance(p, rt.RT_Tuple)
-    assert p == rt.RT_Tuple(4.0, -4.0, 3.0, 0.0)
+    p = tuple.Vector(4, -4, 3)
+    assert isinstance(p, tuple.RT_Tuple)
+    assert p == tuple.RT_Tuple(4.0, -4.0, 3.0, 0.0)
 
 
 def test_add1():
     # Adding two tuples
-    p1 = rt.RT_Tuple(3, -2, 5, 1)
-    p2 = rt.RT_Tuple(-2, 3, 1, 0)
-    assert p1 + p2 == rt.RT_Tuple(1, 1, 6, 1)
+    p1 = tuple.RT_Tuple(3, -2, 5, 1)
+    p2 = tuple.RT_Tuple(-2, 3, 1, 0)
+    assert p1 + p2 == tuple.RT_Tuple(1, 1, 6, 1)
 
 
 def test_sub1():
     # Subtracting two points
-    p1 = rt.Point(3, 2, 1)
-    p2 = rt.Point(5, 6, 7)
-    assert p1 - p2 == rt.Vector(-2, -4, -6)
+    p1 = tuple.Point(3, 2, 1)
+    p2 = tuple.Point(5, 6, 7)
+    assert p1 - p2 == tuple.Vector(-2, -4, -6)
 
 
 def test_sub2():
     # Subtracting a vector from a point
-    p = rt.Point(3, 2, 1)
-    v = rt.Vector(5, 6, 7)
-    assert p - v == rt.Point(-2, -4, -6)
+    p = tuple.Point(3, 2, 1)
+    v = tuple.Vector(5, 6, 7)
+    assert p - v == tuple.Point(-2, -4, -6)
 
 
 def test_sub3():
     # Subtracting two vectors
-    v1 = rt.Vector(3, 2, 1)
-    v2 = rt.Vector(5, 6, 7)
-    assert v1 - v2 == rt.Vector(-2, -4, -6)
+    v1 = tuple.Vector(3, 2, 1)
+    v2 = tuple.Vector(5, 6, 7)
+    assert v1 - v2 == tuple.Vector(-2, -4, -6)
 
 
 def test_negation1():
     # Subtracting a vector from the zero vector
-    zero = rt.Vector(0, 0, 0)
-    v = rt.Vector(1, -2, 3)
-    assert zero - v == rt.Vector(-1, 2, -3)
+    zero = tuple.Vector(0, 0, 0)
+    v = tuple.Vector(1, -2, 3)
+    assert zero - v == tuple.Vector(-1, 2, -3)
 
 
 def test_negation2():
     # Negating a tuple
-    a = rt.RT_Tuple(1, -2, 3, -4)
-    assert -a == rt.RT_Tuple(-1, 2, -3, 4)
+    a = tuple.RT_Tuple(1, -2, 3, -4)
+    assert -a == tuple.RT_Tuple(-1, 2, -3, 4)
 
 
 def test_scalarmult1():
     # Multiplying a tuple by a scalar
-    a = rt.RT_Tuple(1, -2, 3, -4)
-    assert a * 3.5 == rt.RT_Tuple(3.5, -7, 10.5, -14)
+    a = tuple.RT_Tuple(1, -2, 3, -4)
+    assert a * 3.5 == tuple.RT_Tuple(3.5, -7, 10.5, -14)
 
 
 def test_scalarmult2():
     # Multiplying a tuple by a fraction
-    a = rt.RT_Tuple(1, -2, 3, -4)
-    assert a * 0.5 == rt.RT_Tuple(0.5, -1, 1.5, -2)
+    a = tuple.RT_Tuple(1, -2, 3, -4)
+    assert a * 0.5 == tuple.RT_Tuple(0.5, -1, 1.5, -2)
 
 
 def test_tuplemult1():
     # Multiplying a tuple by a tuple
-    a = rt.RT_Tuple(2, 3, 4, 5)
-    b = rt.RT_Tuple(0.5, 4, -1.2, 1.5)
-    assert a * b == rt.RT_Tuple(1, 12, -4.8, 7.5)
+    a = tuple.RT_Tuple(2, 3, 4, 5)
+    b = tuple.RT_Tuple(0.5, 4, -1.2, 1.5)
+    assert a * b == tuple.RT_Tuple(1, 12, -4.8, 7.5)
 
 
 def test_scalardiv1():
     # Dividing a tuple by a scalar
-    a = rt.RT_Tuple(1, -2, 3, -4)
-    assert a / 2 == rt.RT_Tuple(0.5, -1, 1.5, -2)
+    a = tuple.RT_Tuple(1, -2, 3, -4)
+    assert a / 2 == tuple.RT_Tuple(0.5, -1, 1.5, -2)
 
 
 def test_tuplediv1():
     # Dividing a tuple by a tuple
-    a = rt.RT_Tuple(3, 4, 5, 6)
-    b = rt.RT_Tuple(1, 2, -2.5, 4)
-    assert a / b == rt.RT_Tuple(3, 2, -2, 1.5)
+    a = tuple.RT_Tuple(3, 4, 5, 6)
+    b = tuple.RT_Tuple(1, 2, -2.5, 4)
+    assert a / b == tuple.RT_Tuple(3, 2, -2, 1.5)
 
 
 def test_magnitude1():
     # Computing the magnitude of vector (1,0,0)
-    v = rt.Vector(1, 0, 0)
+    v = tuple.Vector(1, 0, 0)
     assert v.magnitude() == 1
 
 
 def test_magnitude2():
     # Computing the magnitude of vector (0,1,0)
-    v = rt.Vector(0, 1, 0)
+    v = tuple.Vector(0, 1, 0)
     assert v.magnitude() == 1
 
 
 def test_magnitude3():
     # Computing the magnitude of vector (0,0,1)
-    v = rt.Vector(0, 0, 1)
+    v = tuple.Vector(0, 0, 1)
     assert v.magnitude() == 1
 
 
 def test_magnitude4():
     # Computing the magnitude of vector (1,2,3)
-    v = rt.Vector(1, 2, 3)
+    v = tuple.Vector(1, 2, 3)
     assert v.magnitude() == math.sqrt(14)
 
 
 def test_magnitude5():
     # Computing the magnitude of vector (-1, -2, -3)
-    v = rt.Vector(-1, -2, -3)
+    v = tuple.Vector(-1, -2, -3)
     assert v.magnitude() == math.sqrt(14)
+
+
+def test_normalize1():
+    # Normalizing vector (4,0,0) gives (1,0,0)
+    v = tuple.Vector(4, 0, 0)
+    assert tuple.normalize(v) == tuple.Vector(1, 0, 0)
+
+
+def test_normalize2():
+    # Normalizing vector (1, 2, 3)
+    v = tuple.Vector(1, 2, 3)
+    assert tuple.normalize(v) == tuple.Vector(0.26726, 0.53452, 0.80178)
+
+
+def test_dot1():
+    # The dot product of two tuples
+    a = tuple.RT_Tuple(1, 2, 3)
+    b = tuple.RT_Tuple(2, 3, 4)
+    assert tuple.dot(a, b) == 20
+
+
+def test_cross1():
+    # The cross product of two vectors
+    a = tuple.Vector(1, 2, 3)
+    b = tuple.Vector(2, 3, 4)
+    assert tuple.cross(a, b) == tuple.Vector(-1, 2, -1)
+    assert tuple.cross(b, a) == tuple.Vector(1, -2, 1)
